798a018e3ffef9357ad0e0034c6ff7a11d81e07a
sinariac <sinariac@gmail.com>
2021-11-25 01:38:21 -0300

    atualização final

--- alien.js
+++ alien.js
@@ -1,9 +1,9 @@
 function ripleyOpcao() {
-  var escolha = prompt("O que Ripley deve fazer? Escolha entre 1 e 2:");
+  var ripleyOpcao = prompt("O que Ripley deve fazer? Escolha entre 1 e 2:");
 
-  if (escolha == 1) {
+  if (ripleyOpcao == 1) {
     location = "./ripleysim1.html";
-  } else if (resposta == 2) {
+  } else if (ripleyOpcao == 2) {
     location = "./ripley3";
   } else {
       alert ("Resposta inválida")
